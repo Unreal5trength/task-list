262398e0d409e2d2d7a823e558b34ecb4ea467fe
Joshua Lebert <expertjosh2000@gmail.com>
2020-08-03 23:37:12 -0400

    main functions work, many bugs that need fixes

--- index.js
+++ index.js
@@ -1,20 +1,119 @@
-let taskList = document.getElementById('taskList');
-let taskInput = document.getElementById('taskInput');
-let addTask = document.getElementById('addTask');
-let tasks = document.getElementsByClassName('tasks');
+const taskList = document.getElementById('taskList');
+const tasks = document.getElementsByClassName('task');
+const createName = document.getElementsByClassName('createName');
+const createBtn = document.getElementsByClassName('createBtn');
 
-let test = document.getElementById('test');
+function deleteTaskElement(btn) {
+    let taskElement = btn.parentNode.parentNode;
+    taskElement.remove();
+}   // once invoked removes the parent of parent of button
+    // task < intask < button is path
 
-function createTask() {
-    let task = document.createElement('div');
-    task.className = 'tasks';
-    taskList.appendChild(task);
+function deleteCreationNode(btn) {
+    let node;
+    if (btn.classList.contains("createBtn")) {
+        node = btn.parentNode;
+    } else if (btn.classList.contains("completeBtn")) {
+        node = btn.parentNode.parentNode.parentNode;  
+    }
+    node.remove();
 }
 
-addTask.addEventListener('click', () => {
-    createTask();
-});
+function createTaskContent(btn) {
+    let task = btn.parentNode.parentNode;
+    let subTask = btn.parentNode;
+    let infoNode = document.createElement('div');
+    task.insertBefore(infoNode, subTask);
+    infoNode.className = "taskInfoNode";
 
-test.addEventListener('click', function() {
+    let completeDiv = document.createElement('div');
+    infoNode.appendChild(completeDiv);
+    completeDiv.className = "completeDiv";
 
+    let completeBtn = document.createElement('button');
+    completeDiv.appendChild(completeBtn);
+    completeBtn.innerHTML = "Complete";
+    completeBtn.className = 'completeBtn';
+    completeBtn.addEventListener('click', () => {
+        deleteCreationNode(completeBtn);
+    });
+
+    let nameDiv = document.createElement('div');
+    infoNode.appendChild(nameDiv);
+    nameDiv.className = "nameDiv";
+    let taskName = document.createElement('h3');
+    nameDiv.appendChild(taskName);
+    taskName.innerHTML = btn.previousElementSibling.value;
+
+    let hr = document.createElement('hr');
+    task.appendChild(hr);
+}
+
+function createTask(btn) {
+    // if btn = false, create a first task
+    let task = btn.parentNode.parentNode;
+    let taskClassList = [];
+    for (let i = 0; i < task.classList.length; i++) {
+        taskClassList.push(task.classList.item(i));
+    };
+    let indent = taskClassList.filter(c => {
+        return c.startsWith("indent");
+    });
+    indent = parseInt(indent[0].slice(6, 7));
+    if (indent > 0 && indent < 3) {
+        let indentedTask = document.createElement('div');
+        task.appendChild(indentedTask);
+        indentedTask.classList = `task ${"indent"+ (indent + 1)}`;
+        createCreationNodes(indentedTask);
+
+        let equalIndentTask = document.createElement('div');
+        task.parentNode.appendChild(equalIndentTask);
+        equalIndentTask.classList = `task indent${indent}`;
+        createCreationNodes(equalIndentTask);
+    } else if (indent === 0) {
+        let indentedTask = document.createElement('div');
+        task.appendChild(indentedTask);
+        indentedTask.classList = `task ${"indent"+ (indent + 1)}`;
+        createCreationNodes(indentedTask);
+
+        let newTask = document.createElement('div');
+        task.parentNode.appendChild(newTask);
+        newTask.classList = `task indent0`;
+        createCreationNodes(newTask);
+    };
+    
+};
+
+function createCreationNodes(task) {
+        let creationNode = document.createElement('div');
+        task.appendChild(creationNode);
+        creationNode.classList = "taskCreationNode";
+
+        let createName = document.createElement('input');
+        creationNode.appendChild(createName);
+        createName.setAttribute("type", "text");
+        createName.setAttribute("placeholder", "Task name...");
+        createName.classList.add("createName");
+
+        let createBtn = document.createElement('button');
+        creationNode.appendChild(createBtn);
+        createBtn.classList.add("createBtn");
+        createBtn.innerHTML = "Create Task";
+        createBtn.addEventListener('click', () => {
+            let btn = event.currentTarget;
+            createTask(btn);
+            createCreationNodes(btn);
+            createTaskContent(btn);
+            deleteCreationNode(btn)
+        });
+        let hr = document.createElement('hr');
+        creationNode.appendChild(hr);
+};
+
+
+createBtn[0].addEventListener('click', (event) => {
+    let btn = event.currentTarget;
+    createTask(btn);
+    createTaskContent(btn);
+    deleteCreationNode(btn);
 });
